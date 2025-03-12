4f23ed61ae506f401cdd15f10f8dfa0eab10ddde
merve-karagulle <mervekrgll44@icloud.com>
2025-03-12 20:22:34 +0300

    yeni dosyaa

---
+++ basicConcepts.cpp
@@ -0,0 +1,12 @@
+int buton=0;
+int sayac=50;
+/*integer deðiþkeni -32768 ile 32768 arasýnda deðer alabilen tam sayýlardýr.
+int arduino iþlemcisinde 2 byte yer kaplar.*/
+const int buton=10;
+/*const deyimi ile buton deðikeni 10 da sabitlenir.daha sonrasýnda deðer deðiþimi yapýlmaya çalýþýrsa 
+program hata verir*/
+#define N 5
+//define boyut kaplamaz.
+//sabit tanýmý yapýlýr.
+//N gördüðü yerde 5 var gibi davranmamýz içindir.
+
